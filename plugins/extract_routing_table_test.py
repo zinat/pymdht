90c91f80a73703ae7bb4ddf0ebfbcdb624d23355
Raul Jimenez <rauljc@kth.se>
2011-11-24 16:34:50 +0100

    extractor data structrure rewriten

--- plugins/extract_routing_table_test.py
+++ plugins/extract_routing_table_test.py
@@ -1,3 +1,4 @@
+import csv
 
 import core.message as message
 from core.node import Node
@@ -7,191 +8,180 @@ import core.ptime as time
 STATUS_OK = 'OK'                 # pinged and give response
 STATUS_TIMEOUT = 'TIMEOUT'       # pinged but fail to response
 STATUS_ERROR = 'ERROR'
-STATUS_ON_PROCESS = 'ON_PROCESS' # to be extracting
-STATUS_COMPLETED = 'COMPLETED' 
+STATUS_PINGING = 'PINGING'
+
+NO_VERSION = '_'
 
 NUM_REPETITIONS = 5
 
+MAX_PARALLEL_EXTRACT = 2
 
+TIMEOUT = 2
+
+
+
+class ExtractingTable(object):
+
+    def __init__(self, node_):
+        self.node = node_
+        self._next_extracting_level = 159
+        self._num_repetitions = 0
+        self._levels = []
+        self.last_fn_query_ts = 0
+
+
+    def add_pnodes(self, pnodes):
+        self._levels.append(pnodes)
+        if len(self._levels) < 2:
+            return
+        for new_pnode in pnodes:
+            for seen_pnode in self._levels[-2]:
+                if new_pnode == seen_pnode:
+                    self._num_repetitions += 1
+                    break
+                
+    def next_level(self):
+        if self._num_repetitions >= NUM_REPETITIONS:
+            return
+        level = self._next_extracting_level
+        self._next_extracting_level -= 1
+        self.last_fn_query_ts = time.time()
+        return level
+
+    def _write(self, csv_file):
+        csv_file.writerow('table')
+        for level in self._levels:
+            csv_file.writerow('level')
+            for pnode in level:
+                csv_file.writerow(pnode.get_csv())
+            csv_file.writerow('elevel')
+
+        csv_file.writerow('etable')
+            
+
+class PingedNode(object):
+
+    def __init__(self, node_):
+        self.node = node_
+        self.rtt = 0
+        self.version = NO_VERSION
+
+    def __eq__(self, other):
+        # nodes are defined per IP address alone!!!
+        return self.node.addr[0] == other.node.addr[0]
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __hash__(self):
+        return self.node.addr[0]
+
+    def get_csv(self):
+        return [self.node.addr[0], str(self.node.addr[1]),
+                repr(self.node.id), self.version, str(self.rtt)]
+
+    
+class ExtractingQueue(object):
+    
+    def __init__(self, msg_f):
+        self.msg_f = msg_f
+        self._to_extract_queue = []      #Node to be extract
+        self._extracting_queue = []      #Node which are extracting      
+        self._all_extracting_ips = set()
+        self._next_snode_to_extract = 0
+        self._csv_file = csv.writer(open('exp_extract.csv', 'w'))
+        
+    def add(self, node_):
+        if node_.addr[0] in self._all_extracting_ips:
+            return #already sampled
+        print 'added'
+        self._all_extracting_ips.add(node_.addr[0])
+        self._to_extract_queue.append(node_)
+
+    def get_fn_queries(self):
+        if self._to_extract_queue and len(
+            self._extracting_queue) < MAX_PARALLEL_EXTRACT:
+            node_ = self._to_extract_queue.pop(0)
+            etable = ExtractingTable(node_)
+            self._extracting_queue.append(etable)
+        current_etable_index = self._next_snode_to_extract
+        etable = self._extracting_queue[self._next_snode_to_extract]
+        self._next_snode_to_extract = (self._next_snode_to_extract
+                                       + 1) % len(self._extracting_queue)
+        level = etable.next_level()
+        queries = []
+        if level:
+            print 'extracting'
+            queries.append(
+                self.msg_f.outgoing_find_node_query(
+                    etable.node, etable.node.id.generate_close_id(level),
+                    experimental_obj=etable))
+        else:
+            print 'extraction done'
+            # extraction done
+            if etable.last_fn_query_ts > TIMEOUT:
+                print 'write'
+                # all pings timed out, write to file
+                etable._write(self._csv_file)
+                del self._extracting_queue[current_etable_index]
+        return queries
+
+    
 class ExperimentalManager:
     def __init__(self, my_id, msg_f):
-        self._node_to_extract = None
+        self.extracting_queue = ExtractingQueue(msg_f)
         self.my_id = my_id
         self.msg_f = msg_f
         
-        
-        self._send_query = True
-        
     def on_query_received(self, msg):
-        find_msgs = []
-        exp_obj = ExpObj()
-        
-        if self._send_query:
-            # We only want to extract from ONE node
-            self._send_query = False 
-            print 'Got query (%s) from  Node  %r ' % (
-                            msg.query ,  msg.src_node)
-            if msg.src_node not in exp_obj.known_nodes:
-                # keep the node in a set called known_nodes 
-                #if it is not known before
-                exp_obj.known_nodes.add(msg.src_node)
-                #1 save the node for extraction
-                self.add_to_extract(msg.src_node,exp_obj)
-                
-                #pop a node and keep it in extracting_queue
-                poped_node = exp_obj.to_extract_queue.pop()
-                #2
-                self.add_to_extracting(poped_node,exp_obj)
-                find_msgs.extend(self.continue_extraction(msg,exp_obj))
-                
-            else:
-                 print "Already known node" 
-        return  find_msgs
-    
-    def continue_extraction(self,msg,exp_obj): 
-        _to_extract_msgs = []
-        print "continue_extracting for node %r " % msg.src_node.ip
-        log_distance = exp_obj.next_log_dist() 
-        target = msg.src_node.id.generate_close_id(log_distance)
-        _node_to_ext = exp_obj.extracting_queue.pop()
-        # command for extraction
-        _to_extract_msgs.append(self.msg_f.outgoing_find_node_query(
-                                                    _node_to_ext,
-                                                    target, None,
-                                                    exp_obj))
-        return _to_extract_msgs
-    
-    def continue_ping(self,node_,exp_obj):
-        ping_msgs = []
-        print 'pinging'
-        ping_msgs.append(self.msg_f.outgoing_ping_query(node_,
-                                                            exp_obj))
-        #print node_ 
-        return ping_msgs
-    
+        self.extracting_queue.add(msg.src_node)
+        find_msgs = self.extracting_queue.get_fn_queries()
+        return find_msgs
+
     def on_response_received(self, msg, related_query):
-        msgs_to_send = []
+        self.extracting_queue.add(msg.src_node)
+        ping_queries = []
+        find_node_queries = []
         exp_obj = related_query.experimental_obj
         if not exp_obj:
-            return
-        else:
-            msgs_to_send.extend(self.does_on_response(msg,exp_obj,
-                                                      related_query))
-        return msgs_to_send
-    
-    def does_on_response(self, msg, exp_obj,related_query):
-        on_response_msg = []
-        ping_msgs = []
+            return []
+
         if related_query.query == message.PING:
-            exp_obj.reg_status_of_node(msg.src_node, STATUS_OK)
-            
+            # exp_obj is a PingedNode
+            exp_obj.status = related_query.rtt
         elif related_query.query == message.FIND_NODE:
-            # keep the node in a set called known_nodes 
-            #if it is not known before
-            for node_ in msg.nodes:
-                exp_obj.known_nodes.add(node_)
-                print "node %r" % node_.ip
-                #3
-                self.add_to_pinging_queue(node_,exp_obj)
-                ping_msgs.extend(self.continue_ping(node_,exp_obj))  
-            
-        exp_obj.print_nodes()    
-        return on_response_msg + ping_msgs
-    
+            # exp_obj is a ExtractingTable
+            pnodes = [PingedNode(node_) for node_ in msg.nodes]
+            exp_obj.add_pnodes(pnodes)
+            ping_queries = [self.msg_f.outgoing_ping_query(
+                    node_, pnode) for pnode in pnodes]
+        return ping_queries + find_node_queries
+
     def on_timeout(self, related_query):
         exp_obj = related_query.experimental_obj
-        if exp_obj:
-            print 'Time out', related_query.query
-            if related_query.query == message.PING:
-                exp_obj.reg_status_of_node(
-                                        related_query.dst_node, 
-                                        STATUS_TIMEOUT)
-            exp_obj.print_nodes()
-    
-    def add_to_extract(self,node_,exp_obj): 
-        exp_obj.save_node_in_to_extract_queue(node_)
-        
-    def add_to_extracting(self,node_,exp_obj):
-        exp_obj.save_node_in_extracting_queue(node_)
-    
-    def add_to_pinging_queue(self,node_,exp_obj):
-        exp_obj.save_node_in_pinging_queue(node_)
-        
-    def add_to_pinged(self,node_,exp_obj):
-        exp_obj.save_node_in_pinged_queue(node_) 
-    
-    def on_error_received(self, msg, related_query):
-        return
-    
-    def on_stop(self):
-        return
-    
-class ExpObj:
-    def __init__(self):
-        
-        # Data Structures to store nodes at different phases
-        self.to_extract_queue = []      #Node to be extract
-        self.extracting_queue = []      #Node which are extracting      
-        self.pinging_queue = []         #Node which are pinging
-         
-        self.known_nodes = set()
-        self.node_status = {}            ##Node , that are pinged saved with their status
-        
-        
-        self.all_ids = set()
-        
-        self.all_ids = set()
-        self.current_log_dist = 160
-        
-    def save_node_in_to_extract_queue(self,node_):
-        self.to_extract_queue.append(node_)
-        print 'Node is In.....to_extract_queue ' , node_.ip
-        
-    
-    def save_node_in_extracting_queue(self,node_):
-        self.extracting_queue.append(node_)
-        print 'Node is In.....to_extracting_queue ' , node_.ip
-        
-    
-    def save_node_in_pinging_queue(self,node_):
-        self.pinging_queue.append(node_)
-        print 'Node is In.....pinging_queue ' , node_.ip
-        
-    
-    def save_node_in_pinged_queue(self,node_):
-        self.pinged_queue.append(node_)
-        print 'Node is In.....pinged_queue ' , node_.ip
-        
-    def next_log_dist(self):
-        self.current_log_dist -= 1
-        return self.current_log_dist
-        
-    def save_bucket(self, log_distance_bucket, nodes): 
-        
-        self.extracted_nodes.append((log_distance_bucket, nodes))  
-        for node_ in nodes:
-            self.all_ids.add(node_.id)  
-            
-    def reg_status_of_node(self, node_, status):
-        self.node_status[node_] = status
-        print"jjjjjjjjjj"
-        
-    def print_nodes(self):
-        total = {}
-        total[STATUS_OK] = 0
-        total[STATUS_TIMEOUT] = 0
-        total[STATUS_ERROR] = 0
-        '''
-        total[STATUS_ON_PROCESS] = 0
-        total[STATUS_COMPLETED] = 0'
-        
-        for logdist, nodes in self.extracted_nodes:
-            print '\nLog Distance = ', logdist'''
-        for node_ in self.pinging_queue:
-            total[self.node_status[node_]] += 1
-            print self.node_status.get(node_)
-        print '\nTotal OK/TIMEOUT/ERROR'
-        print total
+        if not exp_obj:
+            return []
+        if related_query.query == message.PING:
+            exp_obj.status = TIMEOUT
+        elif related_query.query == message.FIND_NODE:
+            # timeout while extracting: retry?????
+            pass
+        return []
 
+    def on_error_received(self, msg, related_query):
+        exp_obj = related_query.experimental_obj
+        if not exp_obj:
+            return []
+        if related_query.query == message.PING:
+            # consider ERROR as TIMEOUT
+            exp_obj.status = TIMEOUT
+        elif related_query.query == message.FIND_NODE:
+            # ERROR while extracting: retry?????
+            pass
+        return []
+
+    def on_stop(self):
+        #TODO: write to file
         
-        
+        return
+    
